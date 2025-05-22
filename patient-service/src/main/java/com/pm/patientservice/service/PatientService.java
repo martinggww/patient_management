b42eab58b8939eebf39c207d9795288d07cef641
Arthur Huang <huangshuo.9@gmail.com>
2025-05-22 12:05:58 -0400

    Update the service layer, return a list of patientResponseDTOs

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,10 +1,12 @@
 package com.pm.patientservice.service;
 
 import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,6 +21,8 @@ public class PatientService {
     public List<PatientResponseDTO> getPatience() {
         List<Patient> patients = patientRepository.findAll();
 
-        // Convert patients to PatientResponseDTO
+        // Inline variables
+        return patients.stream()
+                .map(PatientMapper::toDTO).toList();
     }
 }
